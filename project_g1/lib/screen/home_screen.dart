cf9b44e926cd0f33f8cf9154d217e08d31696307
Natthapong_Pop <popbobkok34@hotmail.com>
2023-03-06 13:59:02 +0700

    add sample button in home_screen

--- project_g1/lib/screen/home_screen.dart
+++ project_g1/lib/screen/home_screen.dart
@@ -10,8 +10,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController latitude = TextEditingController();
+  TextEditingController longtitude = TextEditingController();
+  TextEditingController result = TextEditingController();
+  late int LAT;
+  late int LONG;
+  @override
+  void initState() {
+    super.initState();
+    result.text = "No any Lat and Long input...";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(children: [
+      AppBar(
+        title: Text("HomePage"),
+      ),
+      Center(
+        child: Column(children: [
+          Text(result.text),
+          ElevatedButton(
+              onPressed: () {
+                var latitude = 111;
+                var longtitude = 111;
+                LAT = 111;
+                LONG = 111;
+                setState(() {
+                  result.text =
+                      "Lattitude: ${latitude} // Longtitude: ${longtitude}";
+                });
+              },
+              child: Text("Button-1")),
+          ElevatedButton(
+              onPressed: () {
+                var latitude = 222;
+                var longtitude = 222;
+
+                setState(() {
+                  result.text =
+                      "Lattitude: ${latitude} // Longtitude: ${longtitude}";
+                });
+              },
+              child: Text("Button-2")),
+          ElevatedButton(
+              onPressed: () {
+                var latitude = 333;
+                var longtitude = 333;
+
+                setState(() {
+                  result.text =
+                      "Lattitude: ${latitude} // Longtitude: ${longtitude}";
+                });
+              },
+              child: Text("Button-3")),
+          ElevatedButton(
+              onPressed: () {
+                var latitude = 444;
+                var longtitude = 444;
+
+                setState(() {
+                  result.text =
+                      "Lattitude: ${latitude} // Longtitude: ${longtitude}";
+                });
+              },
+              child: Text("Button-4")),
+        ]),
+      )
+    ]);
   }
 }
