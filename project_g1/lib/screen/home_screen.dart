f7b9cf36ca5c4afeb355e94fa131bc8542271ca6
Natthapong_Pop <popbobkok34@hotmail.com>
2023-03-06 18:54:54 +0700

    button provider setup ***cannnot use

--- project_g1/lib/screen/home_screen.dart
+++ project_g1/lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:project_g1/providers/button_provider.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -13,8 +15,8 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController latitude = TextEditingController();
   TextEditingController longtitude = TextEditingController();
   TextEditingController result = TextEditingController();
-  late int LAT;
-  late int LONG;
+  late double LAT;
+  late double LONG;
   @override
   void initState() {
     super.initState();
@@ -32,6 +34,17 @@ class _MyHomePageState extends State<MyHomePage> {
           Text(result.text),
           ElevatedButton(
               onPressed: () {
+                LAT = 222;
+                LONG = 222;
+
+                setState(() {
+                  result.text = "Lattitude: ${LAT} // Longtitude: ${LONG}";
+                });
+              },
+              child: Text("Button-1")),
+/*
+            
+              onPressed: () {
                 var latitude = 111;
                 var longtitude = 111;
                 LAT = 111;
@@ -42,6 +55,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
               },
               child: Text("Button-1")),
+         
+         */
+
           ElevatedButton(
               onPressed: () {
                 var latitude = 222;
